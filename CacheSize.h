feac04204ae46802a92b00570a1f54447feaf949
rclarke18 <clarkerp@g.cofc.edu>
2020-11-30 22:20:31 -0500

    updated ".h" to have the block size function

--- CacheSize.h
+++ CacheSize.h
@@ -11,5 +11,6 @@
 double fetchMain (void* t);      // pulls data from main memory
 double fetchCache (void* t);	// pulls data from cached memory
 float getSize (void* t);		// loops thru to determine the size of cache 
+float getBSize (void* t);
 
 #endif // CacheSize_impl_h
