701efd3eac0f5fd0f845d89ce6e12f8419935628
rclarke18 <clarkerp@g.cofc.edu>
2020-11-30 22:29:33 -0500

    added the "CacheSize.c" and "CacheSizeProg.c" files. The Prog file is the main file for our program.

---
+++ CacheSize.c
@@ -0,0 +1,16 @@
+#include "CacheSize.h"
+
+
+double fetchMain (void* t);      // pulls data from main memory
+double fetchCache (void* t){	// pulls data from cached memory
+	int i = 5;
+	if(i < 0){
+		return -1.0;
+	}
+}
+float getSize (void* t){		// loops thru to determine the size of cache
+
+} 
+float getBSize (void* t) {
+
+}
