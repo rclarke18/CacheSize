24373daf4e40eb6a4dd1fd3b7d63943e18154b8e
rclarke18 <clarkerp@g.cofc.edu>
2020-12-03 14:11:06 -0500

    README updated with new function idea. CacheSize.c functions updated.

--- CacheSize.c
+++ CacheSize.c
@@ -1,16 +1,49 @@
 #include "CacheSize.h"
 
 
-double fetchMain (void* t);      // pulls data from main memory
-double fetchCache (void* t){	// pulls data from cached memory
-	int i = 5;
-	if(i < 0){
-		return -1.0;
-	}
-}
-float getSize (void* t){		// loops thru to determine the size of cache
+double fetchMain (int* t){      // pulls data from main memory t times
+	int N = 10; // Number of bytes to allocate
+	int *ptr;   // Pointer variable to store address
+	struct timespec ts_begin, ts_end;
+	double elapsed; 
 
-} 
-float getBSize (void* t) {
+	ptr = (int *) malloc(N * sizeof(int));  // Allocate 10 * 4 bytes in memory
+	clock_gettime(CLOCK_MONOTONIC, &ts_begin);
+	for(int i = 0, i < @t, i++){
+
+	}
+	clock_gettime(CLOCK_MONOTONIC, &ts_end);
+	elapsed = ts_end.tv_sec - ts_begin.tv_sec;
+	elapsed += (ts_end.tv_nsec - ts_begin.tv_nsec) / 1000000000.0;
+	return elapsed;
+}
+
+double fetchCache (int* t){	// pulls data from cached memory t times
+	struct timespec ts_begin, ts_end;
+	double elapsed; 
+
+	ptr = (int *) malloc(N * sizeof(int));  // Allocate 10 * 4 bytes in memory
+	clock_gettime(CLOCK_MONOTONIC, &ts_begin);
+	for(int i = 0, i < @t, i++){
+		
+	}
+	clock_gettime(CLOCK_MONOTONIC, &ts_end);
+	elapsed = ts_end.tv_sec - ts_begin.tv_sec;
+	elapsed += (ts_end.tv_nsec - ts_begin.tv_nsec) / 1000000000.0;
+	return elapsed;
+}
+
+
+int getSize (int* t){		// loops thru to determine the size of cache
+	int N = @t; // Number of bytes to allocate
+	int *ptr;   // Pointer variable to store address
+
+	ptr = (int *) malloc(N * sizeof(int));  // Allocate 10 * 4 bytes in memory
+	if(ptr==NULL)  return 0;
+	return ((N)* sizeof(int));
+}
+
+
+float getBSize (void* t) {       // 
 
 }
--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # CacheSize  
 Tool to determine Cache Size and Execution Speed  
 
-## Cached Speed Test Function
+### Cached Speed Test Function
 Calls a cached data value in a comparator  
 We call this function a set amount of times in order to get a more accurate average time of pulling from the cache.  
 
-## Main Memory Speed Test Function
+### Main Memory Speed Test Function
 Calls a new data value from main memory to use in a comparator  
 We call this function a set amount of times in order to get a more accurate average time of pulling from main.  
 
-## Cache Size Funcion
-Explanation  
+### Cache Size Funcion
+Attempts to allocate larger and larger blocks of static memory to see the largest cache size allowed.  
+ - won't work: could hit another cached value and throw smaller array size than actually possible    
 
-## Cache Block Size Function 
+### Cache Block Size Function 
 Explanation  
