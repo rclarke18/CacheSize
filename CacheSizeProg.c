49850d1cee93cb51d4d3d0d662493191919c57ab
rclarke18 <clarkerp@g.cofc.edu>
2020-12-03 17:11:43 -0500

    updated the main program

--- CacheSizeProg.c
+++ CacheSizeProg.c
@@ -2,6 +2,14 @@
 #include "CacheSize.h"
 
 int main(int argc, char** argv){
+	int runs = 10000;
+	pthread_t th_cacheSpeed, th_mainSpeed; 
+
+	pthread_create(&th_cacheSpeed, NULL, fetchCache, &runs);
+	pthread_create(&th_mainSpeed, NULL, fetchMain, &runs);
+
+	pthread_join(th_cacheSpeed, NULL);
+	pthread_join(th_mainSpeed, NULL);
 
 	return 1;
 }
